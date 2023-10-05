ee28627d1b28b30dc0aa1fd72c97b7690879a95b
James1057 <bikerider1999@gmail.com>
2023-10-05 18:03:19 -0500

    Getting user input into a variable

--- ECE441CityBreath/NewScreenPage.xaml.cs
+++ ECE441CityBreath/NewScreenPage.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace ECE441CityBreath
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ECE441CityBreath
 {
     public partial class NewScreenPage : ContentPage
     {
         public NewScreenPage()
         {
             InitializeComponent();
+        }
+       
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = entry.Text;
+
+            myLabel.Text = myText;
+
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+
         }
 
         private void OnNavigateButtonClicked1(object sender, EventArgs e)
