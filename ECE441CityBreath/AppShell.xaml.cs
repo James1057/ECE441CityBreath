3631a55bfe82834c80b1a917e1a425ce45be45cc
James1057 <bikerider1999@gmail.com>
2023-10-05 10:59:42 -0500

    Add project files.

---
+++ ECE441CityBreath/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ECE441CityBreath
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ECE441CityBreath/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ECE441CityBreath
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
