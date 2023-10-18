21e9b59d8703aecec61d019b5bfc7ca4a3315c29
James1057 <bikerider1999@gmail.com>
2023-10-18 18:52:13 -0500

    Major Release. Aes not working yet

---
+++ ECE441CityBreath/AesEncryption.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class AesEncryption
+{
+    public static string EncryptStringToAes256(string plainText, string key, string iv)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Encoding.UTF8.GetBytes(key.Substring(0, 32)); // Take the first 32 bytes of the key
+            aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                }
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    public static string DecryptStringFromAes256(string cipherText, string key, string iv)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Encoding.UTF8.GetBytes(key.Substring(0, 32)); // Take the first 32 bytes of the key
+            aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
--- ECE441CityBreath/NewScreenPage.xaml.cs
+++ ECE441CityBreath/NewScreenPage.xaml.cs
@@ -1,4 +1,11 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
+using System.Data;
+using System;
+using System.Threading.Tasks;
+
 
 namespace ECE441CityBreath
 {
@@ -16,7 +23,7 @@ namespace ECE441CityBreath
             string newText = e.NewTextValue;
             string myText = entryUserName.Text;
 
-            myUserName.Text = myText;
+            //myUserName.Text = myText;
 
         }
 
@@ -26,7 +33,7 @@ namespace ECE441CityBreath
             string newText = e.NewTextValue;
             string myText = entryPassword.Text;
 
-            myPassword.Text = myText;
+            //myPassword.Text = myText;
 
         }
 
@@ -40,6 +47,59 @@ namespace ECE441CityBreath
         {
             // Navigate to the new screen when the button is clicked
             Navigation.PushAsync(new MainPage());
+        }
+
+        private void OnNavigateButtonClickedLogin(object sender, EventArgs e)
+        {
+            //Getting the user name and password
+            String UserName = entryUserName.Text;
+            String PassWord = entryPassword.Text;
+
+            //Make the connection
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = DBUtils.GetDBConnection();
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            Debug.WriteLine(UserName);
+            Debug.WriteLine(PassWord);
+            String valid = "false";
+            try
+            {
+                conn.Open();
+                var stm = "SELECT Username, password FROM Gas.tbl_Users WHERE Username = '" + UserName + "' AND password = '" + PassWord + "' AND Account_Status = 2";
+                var cmd = new MySqlCommand(stm, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    string username = rdr.IsDBNull(0) ? null : rdr.GetString(0);
+                    string password = rdr.IsDBNull(1) ? null : rdr.GetString(1);
+
+                    Debug.WriteLine("{0}, {1}", username, password);
+                    valid = "true";
+                }
+            }
+            catch
+            {
+                //MessageBox.Show("Bad input");
+            }
+
+            // Navigate to the new screen when the button is clicked
+            if (valid == "true")
+            {
+                conn.Close();
+                Navigation.PushAsync(new DashboardPage());
+            }
+            //else
+            //{
+            //    Task.Run(async () =>
+            //    {
+            //        await Task.Delay(5000); // 5000 milliseconds (5 seconds)
+
+            //        // Code to run after the delay (runs on a separate thread)
+            //        // You can't access UI elements directly from here; use the main thread for UI updates if needed
+            //    }).Wait();
+            //}
+            
         }
 
     }
